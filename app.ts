f79f2189f1e06179224c1875fdef61407eca070c
gunnermadsen <gunner.madsen@outlook.com>
2019-06-24 14:16:12 -0700

    testing deployment

--- app.ts
+++ app.ts
@@ -58,9 +58,8 @@ export class PortfolioServer extends Server {
   }
 
   public start(): void {
-    let listenPort = process.env.PORT || '3000';
-    this.app.listen(listenPort, () => {
-      Logger.Info(`Portfolioapis listening on port ${listenPort}`);
+    this.app.listen(process.env.PORT || 3000, () => {
+      Logger.Info(`Portfolioapis listening on port ${process.env.PORT || 3000}`);
     })
   }
 }
