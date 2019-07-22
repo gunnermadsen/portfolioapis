206721bd03cfefc6a0e548b0bcad1ff981ab683d
gunnermadsen <gunner.madsen@outlook.com>
2019-07-22 15:18:08 -0700

    testing deployment with webpack

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { NODE_ENV = 'production' } = process.env;
+
+
+module.exports = {
+    entry: './start.ts',
+    externals: [ nodeExternals() ],
+    mode: NODE_ENV,
+    target: 'node',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'server.[hash].js'
+    },
+    resolve: {
+        extensions: [ '.ts', '.js' ]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: [
+                    'ts-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin()
+    ]
+}
