72bf7fad06228bda93a29e06ed30172d54ec35f6
gunnermadsen <gunner.madsen@outlook.com>
2020-01-03 00:16:58 -0800

    testing deployment

--- start.ts
+++ start.ts
@@ -1,12 +1,12 @@
 import { PortfolioServer } from './app'
 
-declare const module: any
+// declare const module: any
 
 let server = new PortfolioServer()
 
 server.start()
 
-if (module.hot && process.env.NODE_ENV === 'development') {
-    module.hot.accept()
-    module.hot.dispose(() => server.httpServer.close())
-}
+// if (module.hot && process.env.NODE_ENV === 'development') {
+//     module.hot.accept()
+//     module.hot.dispose(() => server.httpServer.close())
+// }
