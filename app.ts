773e4d8d59913e4d773eeb76c447c17c7fb0d316
gunnermadsen <gunner.madsen@outlook.com>
2020-01-02 21:00:54 -0800

    testing deployment

--- app.ts
+++ app.ts
@@ -94,7 +94,6 @@ export class PortfolioServer extends Server {
       response.locals.message = error.message
       response.locals.error = request.app.get('env') === 'development' ? error : {}
       response.status(500).json({ message: error })
-      
     })
   }
 
